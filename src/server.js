5ac0ab2975c361ff5f52506037b53c0d737b8671
anandkamat-tradofina <anand.kamat@tradofina.com>
2025-09-15 23:57:16 +0530

    added all

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import app from './app.js';
+
+const app = express();
+app.use(express.json());
+app.get("/health", (_req, res) => res.json({ ok: true }));
 
 const PORT = process.env.PORT || 4000;
-
-const server = app.listen(PORT, () => {
-  console.log(`API running on http://127.0.0.1:${PORT}`);
-});
-
-process.on('SIGTERM', () => server.close(() => process.exit(0)));
-process.on('SIGINT', () => server.close(() => process.exit(0)));
+app.listen(PORT, () => console.log(`API running on http://127.0.0.1:${PORT}`));
